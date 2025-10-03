4df59fce2662d0b2c0b79d5e588d283b7fc74922
Forud Ghafouri <fzerorubigd@gmail.com>
2025-10-03 16:09:52 +0200

    fix: the solver

--- main.go
+++ main.go
@@ -1,27 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/fzerorubigd/pentomino-solver/psolver"
 )
 
 func main() {
-	cal := psolver.NewPersianCalendar()
-	//mm := psolver.NewMatrix(10, 6)
-	pie := psolver.New12()
+	var W, D, M, Y, count int
+	flag.IntVar(&W, "weekday", 1, "The weekday, 1 for the first day, 7 for 7th day. (Shanbe is first for Persian, Monday for Gregorian)")
+	flag.IntVar(&D, "day", 1, "The day of the month, 1 to 31")
+	flag.IntVar(&M, "month", 1, "The month, 1 to 12")
+	flag.IntVar(&Y, "year", 1, "The year of the calendar, for Persian 1=1404 and for Gregorian 1=2025, max 10")
+	flag.IntVar(&count, "count", -1, "The count of the solution to show before exit, -1 to show all")
+	flag.Parse()
 
-	cal.SetDate(7, 11, 7, 1404)
+	cal := psolver.NewPersianCalendar()
+	pie := psolver.New12()
+	cal.SetDate(W, D, M, Y+1403)
 	resp := make(chan *psolver.Matrix, 10)
-	// go func() {
-	// 	psolver.SolveSingle(&cal.Matrix, pie, resp)
-	// 	close(resp)
-	// }()
 	psolver.Solve(&cal.Matrix, pie, resp)
+
 	i := 1
 	for r := range resp {
 		fmt.Println(i, "===>")
 		fmt.Println(r)
+		if count > 0 && i == count {
+			os.Exit(0)
+		}
 		i += 1
 	}
 }
